effc4838713e5eb0130186132d45067f7ea1243f
ImMin5 <mino@megazone.com>
2024-03-05 16:08:34 +0900

    feat: add description for dashboard

--- src/cloudforet/search/manager/resource_manager.py
+++ src/cloudforet/search/manager/resource_manager.py
@@ -41,6 +41,21 @@ class ResourceManager(BaseManager):
                 if pg_id := result.get("project_group_id"):
                     _name = result.get("name")
                     result["name"] = f"{project_group_map.get(pg_id)} > {_name}"
+        elif resource_type == "dashboard.PublicDashboard":
+            project_ids = self._get_project_ids(results)
+            project_and_project_group_name_map = (
+                self.get_project_and_project_group_name_map(domain_id, project_ids)
+            )
+
+            for result in results:
+                dashboard_type = self._get_dashboard_type(result)
+                if result["project_id"] != "*":
+                    project_path = self._make_project_path(
+                        result["project_id"], project_and_project_group_name_map
+                    )
+                    result["description"] = f"{dashboard_type} ({project_path})"
+                else:
+                    result["description"] = dashboard_type
 
         _LOGGER.debug(f"[search] find_filter: {find_filter}")
         print(
@@ -82,6 +97,38 @@ class ResourceManager(BaseManager):
         return response
 
     @cache.cacheable(
+        key="search:project-map:{domain_id}:{project_ids}",
+        expire=180,
+    )
+    def get_project_and_project_group_name_map(
+        self, domain_id: str, project_ids: list
+    ) -> dict:
+        db_name, collection_name = self._get_collection_and_db_name("identity.Project")
+        project_results = list(
+            self.client[db_name][collection_name].find(
+                {
+                    "domain_id": domain_id,
+                    "project_id": {"$in": project_ids},
+                }
+            )
+        )
+
+        project_group_ids = self._get_project_group_ids(project_results)
+        project_group_map = self.get_project_group_map(domain_id, project_group_ids)
+
+        project_and_project_group_name_map = {
+            project_info["project_id"]: {
+                "project_name": project_info["name"],
+                "project_group_name": project_group_map.get(
+                    project_info.get("project_group_id")
+                ),
+            }
+            for project_info in project_results
+        }
+
+        return project_and_project_group_name_map
+
+    @cache.cacheable(
         key="search:project-group-map:{domain_id}:{project_group_ids}",
         expire=180,
     )
@@ -97,7 +144,9 @@ class ResourceManager(BaseManager):
                 }
             )
         )
-        project_group_map = {pg["project_group_id"]: pg["name"] for pg in response}
+        project_group_map = {
+            pg_info["project_group_id"]: pg_info["name"] for pg_info in response
+        }
 
         return project_group_map
 
@@ -122,14 +171,6 @@ class ResourceManager(BaseManager):
 
         return workspace_owner_workspaces, workspace_member_workspaces
 
-    @staticmethod
-    def _get_project_group_ids(results: list) -> list:
-        project_group_ids = []
-        for result in results:
-            if pg_id := result.get("project_group_id"):
-                project_group_ids.append(pg_id)
-        return project_group_ids
-
     def _get_collection_and_db_name(self, resource_type: str) -> Tuple[str, str]:
         service, resource = resource_type.split(".")
 
@@ -141,6 +182,45 @@ class ResourceManager(BaseManager):
             db_name = service
 
         return db_name, collection_name
+
+    @staticmethod
+    def _get_dashboard_type(result: dict) -> str:
+        dashboard_type = "Workspace"
+
+        if result.get("project_id") != "*":
+            dashboard_type = "Single Project"
+
+        return dashboard_type
+
+    @staticmethod
+    def _get_project_ids(results: list) -> list:
+        project_ids = []
+        for result in results:
+            project_id = result.get("project_id")
+            if project_id and project_id != "*":
+                project_ids.append(project_id)
+        return project_ids
+
+    @staticmethod
+    def _get_project_group_ids(results: list) -> list:
+        project_group_ids = []
+        for result in results:
+            if pg_id := result.get("project_group_id"):
+                project_group_ids.append(pg_id)
+        return project_group_ids
+
+    @staticmethod
+    def _make_project_path(
+        project_id: str, project_and_project_group_name_map: dict
+    ) -> str:
+        project_path_info = project_and_project_group_name_map.get(project_id)
+        if project_group_name := project_path_info.get("project_group_name"):
+            project_path = (
+                f"{project_group_name} > {project_path_info.get('project_name')}"
+            )
+        else:
+            project_path = project_path_info["project_name"]
+        return project_path
 
     @staticmethod
     def _pascal_to_snake_case(pascal_case: str):
--- src/cloudforet/search/model/resource/response.py
+++ src/cloudforet/search/model/resource/response.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 class ResourceResponse(BaseModel):
     resource_id: Union[str, None] = None
     name: Union[str, None] = None
+    description: Union[str, None] = None
     domain_id: str
     workspace_id: Union[str, None] = None
     project_id: Union[str, None] = None
